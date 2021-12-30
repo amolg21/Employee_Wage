2aa71b0ff6a08475d998fb7162493e27547fa532
amol ghotale <amol9.ag@gmail.com>
2021-12-31 00:01:43 +0530

    MODIFIED: Switch Case

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -3,26 +3,36 @@ package com.bridgelabz;
 public class Main {
 
     private static int wage_per_hr = 20;
-    private static int full_day_hr = 8;
-    private static int half_day_hr = 4;
+    private static final int Full_Time = 1;
+    private static final int Part_Time = 2;
 
     public static void main(String[] args) {
-	    // i= Employee Attendance
-        int i = (int)Math.floor(Math.random() * 10) % 3;
+	    /*i= Employee Attendance
+            0=Employee is Absent
+            1=Full time Employee
+            2=Part time employee*/
+
+        int i = (int) Math.floor(Math.random() * 10) % 3;
             System.out.println(i);
+            int day_hr = 0;
 
-            if( i == 1 ) {
+        switch ((int) i) {
+            case Full_Time:
                 System.out.println("Employee is Present for Full Day");
-                int daily_wage = wage_per_hr * full_day_hr;
-                System.out.println(daily_wage);
+                day_hr = 8;
+                break;
 
-            } else if(i == 2) {
+            case Part_Time:
                 System.out.println("Employee is Present for Half Day");
-                int daily_wage = wage_per_hr * half_day_hr;
-                System.out.println(daily_wage);
+                day_hr = 4;
+                break;
 
-            } else {
+            default:
                 System.out.println("Employee is Absent");
+                day_hr = 0;
+                break;
             }
+            int empWages = day_hr * wage_per_hr;
+            System.out.println("Wages= " + empWages);
     }
 }
