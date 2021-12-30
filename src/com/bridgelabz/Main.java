0b04283bf5c003e2d830ec6290f21b5168d4569f
amol ghotale <amol9.ag@gmail.com>
2021-12-30 19:28:08 +0530

    ADDED: daily employee wage

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -2,15 +2,20 @@ package com.bridgelabz;
 
 public class Main {
 
+    private static int wage_per_hr = 20;
+    private static int full_day_hr = 8;
+
     public static void main(String[] args) {
 	    // i= Employee Attendance
         int i = (int)Math.floor(Math.random() * 10) % 2;
             System.out.println(i);
 
-            if( i == 0 ) {
-                System.out.println("Employee is Absent");
-            } else {
+            if( i == 1 ) {
                 System.out.println("Employee is Present");
+                int daily_wage = wage_per_hr * full_day_hr;
+                System.out.println(daily_wage);
+            } else {
+                System.out.println("Employee is Absent");
             }
     }
 }
