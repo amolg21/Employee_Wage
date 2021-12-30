e92e295959f9daa70ecd0dc3a69e3e0b73ddeba5
amol ghotale <amol9.ag@gmail.com>
2021-12-30 19:47:11 +0530

    MODIFIED: Part time wages

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -4,16 +4,23 @@ public class Main {
 
     private static int wage_per_hr = 20;
     private static int full_day_hr = 8;
+    private static int half_day_hr = 4;
 
     public static void main(String[] args) {
 	    // i= Employee Attendance
-        int i = (int)Math.floor(Math.random() * 10) % 2;
+        int i = (int)Math.floor(Math.random() * 10) % 3;
             System.out.println(i);
 
             if( i == 1 ) {
-                System.out.println("Employee is Present");
+                System.out.println("Employee is Present for Full Day");
                 int daily_wage = wage_per_hr * full_day_hr;
                 System.out.println(daily_wage);
+
+            } else if(i == 2) {
+                System.out.println("Employee is Present for Half Day");
+                int daily_wage = wage_per_hr * half_day_hr;
+                System.out.println(daily_wage);
+
             } else {
                 System.out.println("Employee is Absent");
             }
